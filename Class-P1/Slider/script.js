a841a3ce48754554adea77ef77d06fdd86fd4e46
OmidGharib <omidgharib@yahoo.com>
2013-02-16 20:45:49 +0330

    add arrowsicon existence function check every slide ablity for going up , down ,left and right

--- Class-P1/Slider/script.js
+++ Class-P1/Slider/script.js
@@ -1,5 +1,4 @@
 window.onload=function(){
-	var morinji=document.getElementById('morinji');
 	var slider=document.getElementById('slider');
 	var trainer=document.getElementsByClassName('trainer').item(0);
 	var rows =trainer.getElementsByClassName('row');
@@ -52,17 +51,38 @@ window.onload=function(){
 		sum=sum+cols.length;
 	}
 
-	;(go2Slide=function(i,j,obj){
+	;(go2Slide=function(i,j,p){
 		trainer.style.top=(-600*i)+'px';
 		trainer.style.left=(-950*j)+'px';
 		// obj.className='active';
 		// currentSlide.className='';
 		// currentSlide=obj;
 		lists.item(currentSlide).className='';
-		lists.item(obj).className='active';
-		currentSlide=obj;
+		lists.item(p).className='active';
+		currentSlide=p;
+		arrowsIconExistence(i,j); //just for left
 	})(0,0,0);
 
+	function arrowsIconExistence(i,j) { 
+		if(checkNeighborSlides(i,j-1)==false){document.getElementById('left').style.display='none';}
+			else{document.getElementById('left').style.display='block';}
+		if(checkNeighborSlides(i,j+1)==false){document.getElementById('right').style.display='none';}
+			else{document.getElementById('right').style.display='block';}
+		if(checkNeighborSlides(i-1,j)==false){document.getElementById('up').style.display='none';}
+			else{document.getElementById('up').style.display='block';}
+		if(checkNeighborSlides(i+1,j)==false){document.getElementById('down').style.display='none';}
+			else{document.getElementById('down').style.display='block';}
+	}
+
+	function checkNeighborSlides(i,j) {
+		if (i>=0 && j>=0 && i<rows.length) {
+			if(rows.item(i).getElementsByClassName('slide').item(j)!=null) return true;
+			else return false;
+		}
+		else return false;
+	}
+
+	//random and  randomcolors functions
 	function rnd(a,b) {
 		return Math.floor(Math.random()*(b-a))+a;
 	}
@@ -71,7 +91,7 @@ window.onload=function(){
 	var c='#';
 	for(var i=0;i<6;i++){
 		c+=rnd(0,15).toString(16);
-	}
+		}
 		return c;
 	}
 }

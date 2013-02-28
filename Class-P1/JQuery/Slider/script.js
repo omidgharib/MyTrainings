3104186a2d832dafb0bfedffbe48ffa965a02acd
omidgharib <omidgharib@yahoo.com>
2013-02-28 17:52:17 +0330

    add simple next prev btn with jq script

--- Class-P1/JQuery/Slider/script.js
+++ Class-P1/JQuery/Slider/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+	var slider=$("div#slider"),
+	train=$("div#slider > div.train"),
+	slides=$("div#slider > div.train > div.slide"),
+	currentslide=0;
 	
+	(go2slide=function(n){
+		if (n>=slides.length) n=0;
+		if(n<0) n=slides.length-1;
+		train.css("left",-940*n+'px');
+		currentslide=n;
+	})(0);
+
+	$("div#slider > div.next").click(function(){
+		go2slide(currentslide+1);
+	});
+
+	$("div#slider > div.previous").click(function(){
+		go2slide(currentslide-1);
+	});
 });

770461f691986af92fbbdeb32fa5ba4bec283488
Omid Gharib <omidgharib@yahoo.com>
2013-07-11 05:24:09 +0430

    update

---
+++ Class-W4/Test-Theme/Abas/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php bloginfo(); wp_title(); ?></title>
+<link rel="stylesheet" href="<?php blog_info("tempate_url"); ?>\style.css">
+</head>

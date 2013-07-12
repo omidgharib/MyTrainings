08ffa145939827cc2a7b2098fa218892104d96b2
Omid Gharib <omidgharib@yahoo.com>
2013-07-12 16:59:42 +0430

    update read posts

--- Class-W4/Test-Theme/Abas/content.php
+++ Class-W4/Test-Theme/Abas/content.php
@@ -1,3 +1,29 @@
-<div class="container">
-	This is my first theme
+<div>This is my first theme</div>
+<div class="posts">
+	<?php
+		if(have_posts()){
+	?>
+		<?php the_post(); ?>
+			<article>
+				<h1><?php the_title(); ?></h1>
+				<p><?php the_content(); ?></p>
+			</article>
+	<?php 
+		} else{
+	?>
+		<article>
+			<h1>Page Not Found</h1>
+			<p>the requested URL ... </p>
+		</article>
+	<?php
+		}
+	?>
+
+<!-- 	<ul class="nav">
+		<?php
+			wp_list_pages(array(
+				'title_li' => ''
+			));
+		?>
+	</ul> -->
 </div>
--- Class-W4/Test-Theme/Abas/header.php
+++ Class-W4/Test-Theme/Abas/header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title><?php bloginfo(); wp_title(); ?></title>
+<title><?php bloginfo(); wp_title("",true); ?></title>
 <link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/style.css" />
 </head>
 <body>
+<div class="container">

c072d61ce89925e80bc48ba9e48b89d3d91d24d9
Omid Gharib <omidgharib@yahoo.com>
2013-07-25 04:42:22 +0430

    beautifulize

--- Class-W4/Test-Theme/1theme/functions.php
+++ Class-W4/Test-Theme/1theme/functions.php
@@ -1,7 +1,52 @@
 <?php
+function custome_theme_init (){
+	$labels = array(
+	    'name' => _x('product', 'post type general name'),
+	    'singular_name' => _x('product', 'post type singular name'),
+	    'add_new' => _x('Add New', 'product'),
+	    'add_new_item' => __('Add New product'),
+	    'edit_item' => __('Edit product'),
+	    'new_item' => __('New product'),
+	    'view_item' => __('View product'),
+	    'search_items' => __('Search product'),
+	    'not_found' =>  __('No product found'),
+	    'not_found_in_trash' => __('No product found in Trash'),
+	    'parent_item_colon' => '',
+	    'menu_name' => _x('product', 'post type general name')
+	);
 
-add_theme_support('post-thumbnails');
+	$args = array(
+	    'labels' => $labels,
+	    'public' => true,
+	    'publicly_queryable' => true,
+	    'show_ui' => true,
+	    'show_in_menu' => true,
+	    'query_var' => true,
+	    'rewrite' => true,
+	    'capability_type' => 'post',
+	    'has_archive' => true,
+	    'hierarchical' => false,
+	    'menu_position' => 20,
+	    //'menu_icon' => get_bloginfo('template_url') . '/images/menu.png', // 16x16
+	    'supports' => array('title','editor','thumbnail','excerpt')
+	);
 
-add_theme_support('menus');
+	register_post_type ('product',$args);
 
-register_nav_menu('mainmenu', "Main Manu");
+	add_theme_support('post-thumbnails');
+
+	add_theme_support('menus');
+
+	register_nav_menu('mainmenu', "Main Manu");
+
+	register_taxonomy(
+	  	'type',
+	  	'product',
+	  	array(
+			'label' => _x( 'Type',"product taxonomy"),
+	    	'rewrite' => array( 'slug' => 'type' )
+		)
+	);
+}
+
+add_action('init', 'custome_theme_init');

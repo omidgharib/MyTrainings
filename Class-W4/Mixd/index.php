d29f6284b4b5a1e4fd29250d4758f7ed933555ad
Omid Gharib <omidgharib@yahoo.com>
2013-08-15 16:35:56 +0430

    update

--- Class-W4/Mixd/index.php
+++ Class-W4/Mixd/index.php
@@ -1,11 +1,7 @@
 <?php
 	get_template_part('header');
 	get_template_part('head');
-	get_template_part('headafter');
-
 	
-	get_template_part('navigation');
-
 	get_template_part('content');
 
 	get_template_part('foot');
